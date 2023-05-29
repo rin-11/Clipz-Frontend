f599954020d5aee2d3d771a2811fd12c472a9d78
Rin Kates <erinkates11@gmail.com>
2023-05-29 02:23:17 -0400

    onChange for user input added and confirm password state added

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -2,8 +2,20 @@ import React, { useState } from "react";
 import "./Auth.css";
 
 const UserAuth = () => {
-    // set register to false to user is first prompted to log in
-  const [register, setRegister] = useState(false)
+    // handle register/login states
+        // set register to false to user is first prompted to log in
+    const [register, setRegister] = useState(false)
+    // handle user input data (set inputs to null to start)
+    const [userData, setUserData]= useState({email:"", username:"", displayname:"", password:"", confirmpassword:""})
+
+    // check password and confirmpassword are the same value when user hits regsiser
+    const [confirmPassword, setConfirmPassowrd] = useState(true)
+ 
+      // handle user input changes when user hits submit
+  const handleChange = (e)=> {
+    setUserData({...userData, [e.target.name]: e.target.value})
+  }
+
 
   return (
     <div className="UserAuth">
@@ -26,12 +38,14 @@ const UserAuth = () => {
           placeholder="Username"
           className="userInput"
           name="username"
+          onChange = {handleChange}
         />
           <input
           type="text"
           placeholder="Display Name"
           className="userInput"
           name="displayname"
+          onChange = {handleChange}
         />
         </div>
         }
@@ -43,30 +57,38 @@ const UserAuth = () => {
             className="userInput"
             name="email"
             placeholder="Email"
+            onChange = {handleChange}
           />
         </div>
 
         <div>
           <input
-            type="text"
+            type="password"
             className="userInput"
             name="password"
             placeholder="Password"
+            onChange = {handleChange}
           />
           {/* only confirm password if register is true */}
          {register && <input
-            type="text"
+            type="password"
             className="userInput"
             name="confirmpassword"
             placeholder="Confirm Password"
+            onChange = {handleChange}
           />}
         </div>
+        
+        {/* Display for when password do not match and confirmPassword is set to false */}
+        <span style={{ color: "red", fontSize: "12px", alignSelf: "flex-end", marginRight: "5px", display: confirmPassword ? "none" : "block"}}>
+            Passwords Do Not Match
+          </span>
 
         <div>
             {/* onClick using previous value of setRegister to change when clicked to change from register to login forms */}
-        <span style={{fontSize: '12px'}} onClick={()=> setRegister((prev)=>!prev)}>
+        <span style={{fontSize: '12px', cursor:"pointer"}} onClick={()=> setRegister((prev)=>!prev)}>
             {/* Display different texts based on register status */}
-              {register ? "Already have an account? Log In Here": "Don't have an account? Register Here"}
+              {register ? "Already have an account? Click here to login": "Don't have an account? Click here to register"}
               </span>
         </div>
 
