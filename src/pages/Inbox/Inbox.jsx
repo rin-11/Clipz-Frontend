b9961078de8b211c4f97b3dcaf7df49677f9501a
Rin Kates <erinkates11@gmail.com>
2023-06-10 21:28:05 -0400

    inbox file

---
+++ src/pages/Inbox/Inbox.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import "./Inbox.css";
+
+
+
+const Inbox = () => {
+  return (
+    <div className="Inbox">
+
+    </div>
+  )
+}
+
+export default Inbox
