4817e6c49363ede62e51a5528bb942a09b07af1a
Rin Kates <erinkates11@gmail.com>
2023-06-17 10:01:18 -0400

    capstone submission

--- src/components/UserDisplay/EditUser/EditUser.jsx
+++ src/components/UserDisplay/EditUser/EditUser.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import './EditUser.css';
+import { useDispatch } from 'react-redux';
+import { uploadImage } from '../../../actions/UploadAction';
+
 const EditUser = ({ onSave, onCancel }) => {
   const [displayName, setDisplayName] = useState('');
   const [profilePicture, setProfilePicture] = useState(null);
+  const dispatch = useDispatch();
 
   const handleInputChange = (event) => {
     const { name, value, files } = event.target;
@@ -13,8 +17,17 @@ const EditUser = ({ onSave, onCancel }) => {
     }
   };
 
-  const handleSubmit = () => {
-    onSave(displayName, profilePicture);
+  const handleSubmit = async () => {
+    if (profilePicture) {
+      const formData = new FormData();
+      formData.append('image', profilePicture);
+      try {
+        await dispatch(uploadImage(formData));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    onSave(displayName, profilePicture ? profilePicture.name : null);
   };
 
   const handleCancel = () => {
@@ -23,29 +36,35 @@ const EditUser = ({ onSave, onCancel }) => {
 
   return (
     <div className="EditUser">
-        <div className="EditUserDisplay">
-          <h2>Edit Profile</h2>
-          <form>
-            <div className="editDisplayNameForm">
-              <label htmlFor="displayName">Enter Display Name:</label>
-              <input type="text" id="displayName" name="displayName" onChange={handleInputChange} />
-            </div>
-            <div className="editProPicForm">
-              <label htmlFor="profilePicture" className="file-upload">Upload Profile Photo</label>
-              <input type="file" id="profilePicture" name="profilePicture" onChange={handleInputChange} style={{display: 'none'}} />
+      <div className="EditUserDisplay">
+        <h2>Edit Profile</h2>
+        <form>
+          <div className="editDisplayNameForm">
+            <label htmlFor="displayName">Enter Display Name:</label>
+            <input type="text" id="displayName" name="displayName" onChange={handleInputChange} />
           </div>
-
- 
-            <div className="editButtons">
-              <button type="button" className="saveButton" onClick={handleSubmit}>
-                Save
-              </button>
-              <button type="button" className="cancelButton" onClick={handleCancel}>
-                Cancel
-              </button>
-            </div>
-          </form>
-        </div>
+          <div className="editProPicForm">
+            <label htmlFor="profilePicture" className="file-upload">
+              Upload Profile Photo
+            </label>
+            <input
+              type="file"
+              id="profilePicture"
+              name="profilePicture"
+              onChange={handleInputChange}
+              style={{ display: 'none' }}
+            />
+          </div>
+          <div className="editButtons">
+            <button type="button" className="saveButton" onClick={handleSubmit}>
+              Save
+            </button>
+            <button type="button" className="cancelButton" onClick={handleCancel}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/UserDisplay/UserInfo/UserInfo.jsx
+++ src/components/UserDisplay/UserInfo/UserInfo.jsx
@@ -4,101 +4,102 @@ import Edit from '../../../assets/edit.png';
 import Logout from '../../../assets/search.png';
 import './UserInfo.css';
 import EditUser from '../EditUser/EditUser';
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
 import { getUser, updateUser } from '../../../api/UserRequest';
 import { logoutUser } from '../../../actions/AuthAction';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
+import { uploadImage } from '../../../actions/UploadAction';
 
-const UserInfo = ({profilePicture}) => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const { user } = useSelector((state) => state.authReducer.authData);
-    const userId = user?._id;
+const UserInfo = ({ profilePicture }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const userId = user?._id;
 
-    const [userData, setUserData] = useState(null);
-    const [isEditing, setIsEditing] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
 
-    useEffect(() => {
+  useEffect(() => {
     fetchUserData();
-    }, []);
+  }, []);
 
-    const handleLogout = () => {
+  const handleLogout = () => {
     dispatch(logoutUser());
     navigate('/auth');
-    }
+  };
 
-    const fetchUserData = async () => {
+  const fetchUserData = async () => {
     try {
-    if (userId) {
-    const response = await getUser(userId);
-    setUserData(response.data);
-    } else {
-    console.log(user);
-    }
+      if (userId) {
+        const response = await getUser(userId);
+        setUserData(response.data);
+      } else {
+        console.log(user);
+      }
     } catch (error) {
-    console.log(error);
+      console.log(error);
     }
-    };
+  };
 
-    const displayName = userData?.displayName || 'Display Name';
-    const profilePictureSrc = profilePicture ? `/profile/${profilePicture}` : defaultProfilePicture;
-    const username = userData?.username || '@Username';
+  const displayName = userData?.displayName || 'Display Name';
+  const profilePictureSrc = userData?.profilePicture || defaultProfilePicture;
+  const username = userData?.username || '@Username';
 
-    const handleEditClick = () => {
+  const handleEditClick = () => {
     setIsEditing(true);
-    };
+  };
 
-    const handleSave = async (newDisplayName, newProfilePicture) => {
+  const handleSave = async (newDisplayName, newProfilePicture) => {
     try {
-    const updatedData = {
-    displayName: newDisplayName,
-    profilePicture: newProfilePicture || userData.profilePicture,
-    };
-    const response = await updateUser(userId, updatedData);
-    setUserData(response.data);
-    setIsEditing(false);
+      const updatedData = {
+        displayName: newDisplayName,
+        profilePicture: newProfilePicture || userData.profilePicture,
+      };
+      const response = await updateUser(userId, updatedData);
+      setUserData(response.data);
+      setIsEditing(false);
     } catch (error) {
-    console.log(error);
+      console.log(error);
     }
-    };
+  };
 
-    const handleCancel = () => {
+  const handleCancel = () => {
     setIsEditing(false);
-    };
+  };
 
-    if (!userData) {
+  if (!userData) {
     return <div>User Not Found</div>;
-    }
+  }
 
-    return (
-      <div className="UserInfo">
-        <div className="Propic">
-          <img src={profilePictureSrc} alt="Profile Picture" />
-        </div>
-        <div className="ProfileName">
-          <h2>{isEditing ? <EditUser onSave={handleSave} onCancel={handleCancel} /> : displayName}</h2>
-          <h3>{!isEditing && username}</h3>
-          <div className="ProfileButtons">
-            <div className="editButton">
+  return (
+    <div className="UserInfo">
+      <div className="Propic">
+        <img src={profilePictureSrc} alt="Profile Picture" />
+      </div>
+      <div className="ProfileName">
+        <h2>{isEditing ? <EditUser onSave={handleSave} onCancel={handleCancel} /> : displayName}</h2>
+        <h3>{!isEditing && username}</h3>
+        <div className="ProfileButtons">
+          <div className="editButton">
             {!isEditing && (
-            <button className="editButton" onClick={handleEditClick}>
-            <img src={Edit} alt="edit user information" className="editIcon" />
-            <h5>Edit Profile</h5>
-            </button>
+              <button className="editButton" onClick={handleEditClick}>
+                <img src={Edit} alt="edit user information" className="editIcon" />
+                <h5>Edit Profile</h5>
+              </button>
             )}
-            </div>
+          </div>
           {!isEditing && (
             <div className="logoutButton">
               <button className="logoutButton" onClick={handleLogout}>
-              <img src={Logout} alt="Logout" className="logoutIcon" />
-              <h4>Logout</h4>
+                <img src={Logout} alt="Logout" className="logoutIcon" />
+                <h4>Logout</h4>
               </button>
             </div>
           )}
-          </div>
         </div>
       </div>
-    );
-    };
+    </div>
+  );
+};
 
 export default UserInfo;
